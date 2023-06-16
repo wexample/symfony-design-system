555b1ccab364c4c945846b8141b72e29c74de001
weeger <romain.weeger@gmail.com>
2023-06-16 12:27:54 +0200

    Core bundle

---
+++ src/DependencyInjection/Compiler/DesignSystemTemplatesCompilerPass.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wexample\SymfonyDesignSystem\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class DesignSystemTemplatesCompilerPass implements CompilerPassInterface
+{
+    public function __construct(
+        private string $frontPath,
+        private string $alias,
+    ) {
+
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $definition = $container->getDefinition('twig.loader.native_filesystem');
+
+        $definition->addMethodCall(
+            'addPath',
+            [
+                $this->frontPath,
+                $this->alias,
+            ]
+        );
+    }
+}
--- src/WexampleSymfonyDesignSystemBundle.php
+++ src/WexampleSymfonyDesignSystemBundle.php
@@ -1,10 +1,20 @@
 <?php
 
-namespace Wexample\SymfonyApi;
+namespace Wexample\SymfonyDesignSystem;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Wexample\SymfonyDesignSystem\DependencyInjection\Compiler\DesignSystemTemplatesCompilerPass;
 
 class WexampleSymfonyDesignSystemBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(
+            new DesignSystemTemplatesCompilerPass(
+                __DIR__.'/../../../front',
+                'SymfonyDesignSystemBundle'
+            )
+        );
+    }
 }
