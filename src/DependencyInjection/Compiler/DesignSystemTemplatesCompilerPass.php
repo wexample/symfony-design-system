713c871e4d1fad1c8f475a3dae8301d62499574c
weeger <romain.weeger@gmail.com>
2024-02-05 10:48:06 +0100

    Avoid fronts folder from bundles

--- src/DependencyInjection/Compiler/DesignSystemTemplatesCompilerPass.php
+++ src/DependencyInjection/Compiler/DesignSystemTemplatesCompilerPass.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Wexample\SymfonyDesignSystem\Helper\DesignSystemHelper;
 use Wexample\SymfonyHelpers\AbstractBundle;
+use Wexample\SymfonyHelpers\Helper\VariableHelper;
 
 readonly class DesignSystemTemplatesCompilerPass implements CompilerPassInterface
 {
@@ -20,23 +21,27 @@ readonly class DesignSystemTemplatesCompilerPass implements CompilerPassInterfac
          */
         foreach ($bundlesPaths as $bundleClass => $paths) {
             foreach ($paths as $path) {
-                $definition->addMethodCall(
-                    'addPath',
-                    [
-                        $path,
-                        class_exists($bundleClass) ?
-                            $bundleClass::getAlias() : $bundleClass,
-                    ]
-                );
-
-                // Add also to allow find all "front" folder, as in translations extension.
-                $definition->addMethodCall(
-                    'addPath',
-                    [
-                        $path,
-                        DesignSystemHelper::TWIG_NAMESPACE_FRONT,
-                    ]
-                );
+                if ($bundleClass != VariableHelper::APP) {
+                    # Add template alias like @SymfonyDesignSystem for every registered path.
+                    $definition->addMethodCall(
+                        'addPath',
+                        [
+                            $path,
+                            class_exists($bundleClass) ?
+                                $bundleClass::getAlias() : $bundleClass,
+                        ]
+                    );
+                }
+                else {
+                    // Add also to allow find all "front" folder, as in translations extension.
+                    $definition->addMethodCall(
+                        'addPath',
+                        [
+                            $path,
+                            basename($path),
+                        ]
+                    );
+                }
             }
         }
     }
