1470c89b65fc7792a1a059bb69f2c8bfcee0b082
weeger <romain.weeger@gmail.com>
2024-02-07 15:27:14 +0100

    Remove declarations

---
+++ assets/js/class/Component.ts
@@ -0,0 +1,5 @@
+import RenderNode from './RenderNode';
+
+export default abstract class Component extends RenderNode {
+
+}
--- assets/js/class/RenderNode.ts
+++ assets/js/class/RenderNode.ts
@@ -3,4 +3,7 @@ import AppChild from './AppChild';
 export default abstract class RenderNode extends AppChild {
   public el: HTMLElement;
 
+  protected async mounted(): Promise<void> {
+
+  }
 }
