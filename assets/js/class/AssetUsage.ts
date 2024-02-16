9b7098ed981206301d824956175f1735518dec1b
weeger <romain.weeger@gmail.com>
2024-02-16 10:43:36 +0100

    New usage in frontend

--- assets/js/class/AssetUsage.ts
+++ assets/js/class/AssetUsage.ts
@@ -3,15 +3,15 @@ import AssetsInterface from '../interfaces/AssetInterface';
 import RenderNode from './RenderNode';
 
 export default abstract class AssetUsage extends AppChild {
-  public static USAGE_ANIMATION: string = 'animation';
+  public static USAGE_ANIMATIONS: string = 'animations';
 
   public static USAGE_COLOR_SCHEME: string = 'color_scheme';
 
   public static USAGE_DEFAULT: string = 'default';
 
-  public static USAGE_RESPONSIVE: string = 'responsive';
+  public static USAGE_MARGINS: string = 'margins';
 
-  public static USAGE_SHAPE: string = 'shape';
+  public static USAGE_RESPONSIVE: string = 'responsive';
 
   public static USAGES: string[] = [
     // The order is the same as backend order.
@@ -19,16 +19,17 @@ export default abstract class AssetUsage extends AppChild {
     AssetUsage.USAGE_DEFAULT,
     AssetUsage.USAGE_COLOR_SCHEME,
     AssetUsage.USAGE_RESPONSIVE,
-    AssetUsage.USAGE_SHAPE,
-    AssetUsage.USAGE_ANIMATION,
+    AssetUsage.USAGE_ANIMATIONS,
   ];
 
   public abstract usageName: string;
 
   public assetShouldBeLoaded(
     asset: AssetsInterface,
-    renderNode: RenderNode
+    renderNode?: RenderNode
   ): boolean {
-    return asset.usage === this.usageName;
+    return asset.usage === this.usageName
+      && renderNode
+      && asset.usages[this.usageName] == renderNode.usages[this.usageName];
   }
 }
