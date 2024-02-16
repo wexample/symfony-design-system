b96fe8e9342a2c4c3cad3ae00067083c942cff49
weeger <romain.weeger@gmail.com>
2024-02-16 10:46:14 +0100

    Demo switch

--- assets/pages/demo/color_schemes.ts
+++ assets/pages/demo/color_schemes.ts
@@ -4,11 +4,12 @@ import Events from'@wexample/symfony-design-system/js/helpers/Events';
 export default class extends Page {
   async pageReady() {
     document
-      .querySelectorAll('.demo-button-switch-color-scheme')
+      .querySelectorAll('.demo-button-switch-usage')
       .forEach((el) => {
         el.addEventListener(Events.CLICK, async () => {
-          await this.app.layout.colorSchemeSet(
-            el.getAttribute('data-color-scheme'),
+          await this.app.layout.setUsage(
+            el.getAttribute('data-usage-name'),
+            el.getAttribute('data-usage-value'),
             true
           );
         });
