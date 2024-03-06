887c29cbe4616212b936ebc54abb9cbbe0d924d1
weeger <romain.weeger@gmail.com>
2024-03-06 14:30:18 +0100

    Fa support

--- src/Twig/IconExtension.php
+++ src/Twig/IconExtension.php
@@ -41,64 +41,9 @@ class IconExtension extends AbstractExtension
     ) {
         $this->projectDir = $kernel->getProjectDir();
         $this->icons = (object) [
+            self::ICONS_LIBRARY_FA => $this->buildIconsListFa(),
             self::ICONS_LIBRARY_MATERIAL => $this->buildIconsListMaterial(),
         ];
-    }
-
-    public function buildIconsListMaterial(): array
-    {
-        $output = [];
-        $pathFonts = $this->projectDir.'/node_modules/material-design-icons/';
-
-        foreach (scandir($pathFonts) as $item) {
-            $svgDir = $pathFonts.$item.'/svg/production/';
-            if ($item[0] !== '.' && is_dir($pathFonts.$item) && file_exists($svgDir)) {
-                foreach (scandir($svgDir) as $iconFile) {
-                    if ($iconFile[0] !== '.') {
-                        $prefix = 'ic_';
-                        $suffix = '_24px.svg';
-                        if (str_starts_with($iconFile, $prefix) && str_ends_with($iconFile, $suffix)) {
-                            $iconName = TextHelper::removePrefix(
-                                TextHelper::removeSuffix(
-                                    $iconFile,
-                                    $suffix
-                                ),
-                                $prefix
-                            );
-
-                            $output[$iconName] = [
-                                'content' => null,
-                                'file' => $svgDir.$iconFile,
-                                'name' => $iconName,
-                            ];
-                        }
-                    }
-                }
-            }
-        }
-
-        return $output;
-    }
-
-    public function buildIconsListFa(): array
-    {
-        $pathSvg = $this->projectDir.'/node_modules/@fortawesome/fontawesome-free/svgs/';
-        $groups = scandir($pathSvg);
-        $output = [];
-
-        foreach ($groups as $group) {
-            if ('.' !== $group[0]) {
-                $icons = scandir($pathSvg.$group);
-                foreach ($icons as $icon) {
-                    if ('.' !== $icon[0]) {
-                        $iconName = FileHelper::removeExtension(basename($icon));
-                        $output[$group][$iconName] = $iconName;
-                    }
-                }
-            }
-        }
-
-        return $output;
     }
 
     public function getFunctions(): array
@@ -125,14 +70,74 @@ class IconExtension extends AbstractExtension
         ];
     }
 
+    public function buildIconsListMaterial(): array
+    {
+        $output = [];
+        $pathFonts = $this->projectDir.'/node_modules/material-design-icons/';
+
+        foreach (scandir($pathFonts) as $item) {
+            $pathSvg = $pathFonts.$item.'/svg/production/';
+            if ($item[0] !== '.' && is_dir($pathFonts.$item) && file_exists($pathSvg)) {
+                foreach (scandir($pathSvg) as $fileIcon) {
+                    if ($fileIcon[0] !== '.') {
+                        $prefix = 'ic_';
+                        $suffix = '_24px.svg';
+                        if (str_starts_with($fileIcon, $prefix) && str_ends_with($fileIcon, $suffix)) {
+                            $iconName = TextHelper::removePrefix(
+                                TextHelper::removeSuffix(
+                                    $fileIcon,
+                                    $suffix
+                                ),
+                                $prefix
+                            );
+
+                            $output[$iconName] = [
+                                'content' => null,
+                                'file' => $pathSvg.$fileIcon,
+                                'name' => $iconName,
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $output;
+    }
+
+    public function buildIconsListFa(): array
+    {
+        $pathSvg = $this->projectDir.'/node_modules/@fortawesome/fontawesome-free/svgs/';
+        $groups = scandir($pathSvg);
+        $output = [];
+
+        foreach ($groups as $group) {
+            if ('.' !== $group[0]) {
+                $icons = scandir($pathSvg.$group);
+                foreach ($icons as $fileIcon) {
+                    if ('.' !== $fileIcon[0]) {
+                        $iconName = $group.'/'.FileHelper::removeExtension(basename($fileIcon));
+                        $output[$iconName] = [
+                            'name' => $iconName,
+                            'file' => $pathSvg.$group.'/'.$fileIcon,
+                            'content' => null,
+                        ];
+                    }
+                }
+            }
+        }
+
+        return $output;
+    }
+
     /**
      * @throws Exception
      */
     public function icon(
         Environment $twig,
         string $name,
-        string $class = '',
-        string $tagName = 'i'
+        bool $global = false,
+        array $classes = []
     ): string {
         $type = null;
 
@@ -143,43 +148,55 @@ class IconExtension extends AbstractExtension
             );
         }
 
-        $class .= ' icon ';
+        $default = DomHelper::buildTag('span');
+
         // Materialize.
         if (self::ICONS_LIBRARY_MATERIAL === $type || (null === $type && isset($this->icons->material[$name]))) {
-            $default = DomHelper::buildTag('span');
+            return $this->loadIconSvg($this->icons->material, $name, $classes) ?: $default;
+        }
 
-            if (isset($this->icons->material[$name])) {
-                if (is_null($this->icons->material[$name]['content'])) {
-                    $svgContent = file_get_contents($this->icons->material[$name]['file']);
-
-                    $dom = new DOMDocument();
-                    $dom->loadXML($svgContent);
-                    $tags = $dom->getElementsByTagName('svg');
-                    if ($tags->length > 0) {
-                        $svg = $tags->item(0);
-
-                        $existingClass = $svg->getAttribute('class');
-                        $newClass = $existingClass ? $existingClass.' mdc-button__icon' : 'mdc-button__icon';
-                        $svg->setAttribute('class', $newClass);
-
-                        $content = $dom->saveXML($svg);
-                    } else {
-                        $content = $default;
-                    }
-
-                    $this->icons->material[$name]['content'] = $content;
-                }
-
-                return $this->icons->material[$name]['content'];
-            }
-
-            return $default;
+        // Font Awesome.
+        if (self::ICONS_LIBRARY_FA === $type || (null === $type && isset($this->icons->fa->$name))) {
+            return $this->loadIconSvg($this->icons->fa, $name, $classes) ?: $default;
         }
 
         // Just display tag on error.
-        return DomHelper::buildTag($tagName, [
-            VariableHelper::CLASS_VAR => $class,
-        ]);
+        return $default;
+    }
+
+    private function loadIconSvg(
+        array &$registry,
+        string $name,
+        array $classes
+    ): ?string {
+        if (isset($registry[$name])) {
+            if (is_null($registry[$name]['content'])) {
+                $svgContent = file_get_contents($registry[$name]['file']);
+
+                $dom = new DOMDocument();
+                $dom->loadXML($svgContent);
+                $tags = $dom->getElementsByTagName('svg');
+                if ($tags->length > 0) {
+                    $svg = $tags->item(0);
+
+                    $existingClass = $svg->getAttribute('class');
+                    $svg->setAttribute(
+                        'class',
+                        $existingClass.implode(' ',
+                            array_merge([
+                                'icon',
+                            ], $classes)));
+
+                    $content = $dom->saveXML($svg);
+
+                    $registry[$name]['content'] = $content;
+
+                    return $registry[$name]['content'];
+                }
+            }
+        }
+
+        return null;
     }
 
     /**
@@ -195,16 +212,5 @@ class IconExtension extends AbstractExtension
         }
 
         return [];
-    }
-
-    private function findFaIconGroup(string $name): ?string
-    {
-        foreach ($this->icons->fa as $group => $icons) {
-            if (isset($icons[$name])) {
-                return $group;
-            }
-        }
-
-        return null;
     }
 }
