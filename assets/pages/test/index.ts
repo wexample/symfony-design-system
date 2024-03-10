24bdd80de84b22a371e3e0e45774fabdc3436cb6
weeger <romain.weeger@gmail.com>
2024-03-10 08:12:26 +0100

    Active test aggregation

--- assets/pages/test/index.ts
+++ assets/pages/test/index.ts
@@ -1,26 +1,34 @@
 import AggregationTest from './class/AggregationTest';
 import AdaptiveRenderingTest from './class/AdaptiveRenderingTest';
-import TestManagerPage from '../../../js/class/TestManagerPage';
-import TranslationTest from './class/TranslationTest';
-import TestTest from './class/TestTest';
 import AppTest from './class/AppTest';
+import HelperTest from './class/HelperTest';
+import IconTest from './class/IconTest';
+import ModalInModalTest from './class/ModalInModalTest';
+import NoJsTest from './class/NoJsTest';
+import OverlayTest from './class/OverlayTest';
 import ResponsiveTest from './class/ResponsiveTest';
+import TestTest from './class/TestTest';
+import TranslationTest from './class/TranslationTest';
+import UsageTest from './class/UsageTest';
 import VariablesTest from './class/VariablesTest';
-import NoJsTest from "./class/NoJsTest";
+import TestManagerPage from '../../js/class/TestManagerPage';
 
 
 export default class extends TestManagerPage {
   async pageReady() {
-    // TODO Test overlays (multiple / inside a modal ?)
-
     await this.runTests({
       AggregationTest,
       AdaptiveRenderingTest,
       AppTest,
+      IconTest,
+      HelperTest,
+      ModalInModalTest,
       NoJsTest,
+      OverlayTest,
       ResponsiveTest,
       TestTest,
       TranslationTest,
+      UsageTest,
       VariablesTest,
     });
 
