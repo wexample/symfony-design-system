ccbff475e41ef2dd543c55d0b5fb1cef9fba9533
weeger <romain.weeger@gmail.com>
2024-03-12 21:22:25 +0100

    Test translations

--- assets/js/helpers/Objects.ts
+++ assets/js/helpers/Objects.ts
@@ -67,3 +67,13 @@ export function deepAssignWithOptions(options) {
     return target;
   };
 }
+
+export function callPrototypeMethodIfExists(self, methodName: string, args = {}) {
+  const method = Object.getPrototypeOf(self)[methodName];
+
+  if (method) {
+    return method.apply(self, args);
+  }
+
+  return undefined;
+}
