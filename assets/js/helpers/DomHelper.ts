bfbae1f49e3583f21827fb2e3b924b14015b2aff
weeger <romain.weeger@gmail.com>
2024-03-12 21:20:19 +0100

    Add all helpers

--- assets/js/helpers/DomHelper.ts
+++ assets/js/helpers/DomHelper.ts
@@ -38,3 +38,44 @@ export function findPreviousNode(el: HTMLElement) {
   } while (el && el.nodeType === Node.TEXT_NODE);
   return el;
 }
+
+/**
+ * Return first scrollable parent.
+ *
+ * @see https://stackoverflow.com/a/42543908/2057976
+ * @param element
+ * @param includeHidden
+ * @returns {HTMLElement}
+ */
+export function findScrollParent(element, includeHidden) {
+  let style = getComputedStyle(element);
+  let excludeStaticParent = style.position === 'absolute';
+  let overflowRegex = includeHidden
+    ? /(auto|scroll|hidden)/
+    : /(auto|scroll)/;
+
+  if (style.position === 'fixed') return document.body;
+  for (let parent = element; (parent = parent.parentElement);) {
+    style = getComputedStyle(parent);
+    if (excludeStaticParent && style.position === 'static') {
+      continue;
+    }
+    if (
+      overflowRegex.test(
+        style.overflow + style.overflowY + style.overflowX
+      )
+    )
+      return parent;
+  }
+
+  return document.body;
+}
+
+export function toggleMainOverlay(bool = null) {
+  let classList = document.getElementById('main-overlay').classList;
+
+  // Detect toggle direction.
+  bool = bool !== null ? bool : !classList.contains('visible');
+
+  classList[bool ? 'add' : 'remove']('visible');
+}
--- assets/js/helpers/StringHelper.ts
+++ assets/js/helpers/StringHelper.ts
@@ -1,3 +1,11 @@
+export function firstLetterLowerCase(string: string): string {
+  return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+export function firstLetterUpperCase(string: string): string {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export function format(text: string, args: object): string {
   Object.entries(args).forEach((data) => {
     let reg = new RegExp(data[0], 'g');
@@ -7,8 +15,18 @@ export function format(text: string, args: object): string {
   return text;
 }
 
+export function toCamel(string: string): string {
+  return firstLetterLowerCase(
+    string.replace(/([\_\-]\w)/g, (m) => m[1].toUpperCase())
+  );
+}
+
 export function toKebab(string: string): string {
   return string.replace(/[\_\-]/g, '-').toLowerCase();
+}
+
+export function toScreamingSnake(string: string): string {
+  return toKebab(string).replace(/-/g, '_').toUpperCase();
 }
 
 export function pathToTagName(string: string): string {
