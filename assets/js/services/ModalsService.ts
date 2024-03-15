efaa889f08f6be440e2fff69c0180f2ec369c612
weeger <romain.weeger@gmail.com>
2024-03-15 10:40:07 +0100

    Show modal

---
+++ assets/js/services/ModalsService.ts
@@ -0,0 +1,17 @@
+import AppService from '../class/AppService';
+import ModalInterface from '../interfaces/RequestOptions/ModalInterface';
+import PagesService from './PagesService';
+
+export default class ModalsService extends AppService {
+  public static dependencies: typeof AppService[] = [PagesService];
+  public services: {
+    pages: PagesService;
+  };
+  public static serviceName: string = 'modals';
+
+  get(path: string, requestOptions: ModalInterface = {}): Promise<any> {
+    requestOptions.layout = requestOptions.layout || 'modal';
+
+    return this.app.services.adaptive.get(path, requestOptions);
+  }
+}
