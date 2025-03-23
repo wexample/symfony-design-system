bd9bf0febfa295a66d3ad047d8dcaa43d6ea90de
weeger <romain.weeger@gmail.com>
2025-03-23 17:21:10 +0100

    Helpers

--- src/Helper/TemplateHelper.php
+++ src/Helper/TemplateHelper.php
@@ -8,11 +8,20 @@ class TemplateHelper
 {
     public const TEMPLATE_FILE_EXTENSION = '.html.twig';
 
+    protected const string VIEW_PATH_PREFIX = '@';
+
     public static function removeExtension(string $viewPAthWithExtension): string
     {
         return TextHelper::removeSuffix(
             $viewPAthWithExtension,
             self::TEMPLATE_FILE_EXTENSION
         );
+    }
+
+    public static function trimPathPrefix(
+        string $domain,
+    ): string
+    {
+        return substr($domain, strlen(self::VIEW_PATH_PREFIX));
     }
 }
