f1644b9f1f71034dcc87c7eaf9ffe4220d1d6a4f
weeger <romain.weeger@gmail.com>
2025-04-14 22:08:30 +0200

    Views names

--- src/Rendering/Traits/WithView.php
+++ src/Rendering/Traits/WithView.php
@@ -4,7 +4,7 @@ namespace Wexample\SymfonyDesignSystem\Rendering\Traits;
 
 trait WithView
 {
-    private ?string $view = null;
+    protected ?string $view = null;
 
     public function getView(): ?string
     {
