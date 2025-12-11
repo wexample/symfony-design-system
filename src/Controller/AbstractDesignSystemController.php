416ccb659bd67226106d366aa6acd738b9f84508
weeger <romain.weeger@gmail.com>
2025-12-11 18:43:33 +0100

    Comment

--- src/Controller/AbstractDesignSystemController.php
+++ src/Controller/AbstractDesignSystemController.php
@@ -8,6 +8,9 @@ use Wexample\SymfonyHelpers\Helper\TemplateHelper;
 
 abstract class AbstractDesignSystemController extends AbstractController
 {
+    /**
+     * @return string Allow bundle-specific front template directories.
+     */
     public static function getTemplateLocationPrefix(): string
     {
         $bundleClass = static::getControllerBundle();
@@ -15,6 +18,14 @@ abstract class AbstractDesignSystemController extends AbstractController
         return ($bundleClass ? $bundleClass::getAlias() : DesignSystemHelper::TWIG_NAMESPACE_FRONT);
     }
 
+    /**
+     * Based on the controller name, find the matching template dir.
+     * The controller and its templates should follow the same directories structure.
+     * ex:
+     *   - Config/DesignSystem/AppController.php
+     *   - config/design_system/app/(index.html.twig)
+     * @return string
+     */
     public static function getControllerTemplateDir(): string
     {
         return TemplateHelper::joinNormalizedParts(
