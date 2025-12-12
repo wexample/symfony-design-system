c8e97e6eb16bc7dd79a8b6b73d8a0bfdb3bdc77c
weeger <romain.weeger@gmail.com>
2025-12-12 17:49:32 +0100

    Asset manager

--- src/Service/AssetsService.php
+++ src/Service/AssetsService.php
@@ -3,10 +3,16 @@
 namespace Wexample\SymfonyDesignSystem\Service;
 
 use Wexample\SymfonyDesignSystem\Rendering\RenderPass;
+use Wexample\WebRenderNode\Asset\AssetManager;
 use Wexample\WebRenderNode\Rendering\RenderNode\AbstractRenderNode;
 
-class AssetsService
+class AssetsService extends AssetManager
 {
+    public function getUsages(): array
+    {
+        return [];
+    }
+
     public function assetsDetect(
         RenderPass $renderPass,
         AbstractRenderNode $renderNode,
