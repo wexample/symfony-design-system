9fbb25d7cc39d7bd9979f3081a54f44ebd0027c7
weeger <romain.weeger@gmail.com>
2025-12-13 13:53:45 +0100

    Test

--- tests/Fixtures/App/AppKernel.php
+++ tests/Fixtures/App/AppKernel.php
@@ -2,9 +2,8 @@
 
 namespace Wexample\SymfonyDesignSystem\Tests\Fixtures\App;
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -12,14 +11,17 @@ class AppKernel extends Kernel
     {
         return [
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Wexample\SymfonyDesignSystem\WexampleSymfonyDesignSystemBundle(),
+            new \Wexample\SymfonyHelpers\WexampleSymfonyHelpersBundle(),
+            new \Wexample\SymfonyTranslations\WexampleSymfonyTranslationsBundle(),
         ];
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
-        $loader->load(__DIR__.'/config/config.yaml');
+        $loader->load(__DIR__ . '/config/config.yaml');
     }
 
     public function getProjectDir(): string
