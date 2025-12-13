0f18b184000da03213c79b6a9fa4db9ab9e08ecc
weeger <romain.weeger@gmail.com>
2025-12-13 14:20:08 +0100

    Kernel test case

--- tests/Fixtures/App/AppKernel.php
+++ tests/Fixtures/App/AppKernel.php
@@ -3,25 +3,26 @@
 namespace Wexample\SymfonyDesignSystem\Tests\Fixtures\App;
 
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Wexample\SymfonyTesting\Tests\TestKernel;
 
-class AppKernel extends Kernel
+class AppKernel extends TestKernel
 {
     public function registerBundles(): iterable
     {
-        return [
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new \Symfony\Bundle\TwigBundle\TwigBundle(),
-            new \Wexample\SymfonyDesignSystem\WexampleSymfonyDesignSystemBundle(),
-            new \Wexample\SymfonyHelpers\WexampleSymfonyHelpersBundle(),
-            new \Wexample\SymfonyTranslations\WexampleSymfonyTranslationsBundle(),
-        ];
+        yield from parent::registerBundles();
+
+        yield new \Wexample\SymfonyDesignSystem\WexampleSymfonyDesignSystemBundle();
+        yield new \Wexample\SymfonyTranslations\WexampleSymfonyTranslationsBundle();
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader): void
-    {
-        $loader->load(__DIR__ . '/config/config.yaml');
+    protected function configureContainer(
+        ContainerBuilder $container,
+        LoaderInterface $loader
+    ): void {
+        parent::configureContainer($container, $loader);
+
+        $loader->load(__DIR__.'/config/config.yaml');
     }
 
     public function getProjectDir(): string
