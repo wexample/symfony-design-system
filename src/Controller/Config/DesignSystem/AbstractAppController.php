15d94e319f399c371a6a08d23e125d5a5a95e5cb
weeger <romain.weeger@gmail.com>
2025-12-13 08:26:24 +0100

    Render node

--- src/Controller/Config/DesignSystem/AbstractAppController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wexample\SymfonyDesignSystem\Controller\Config\DesignSystem;
-
-use Wexample\SymfonyDesignSystem\Controller\AbstractPagesController;
-
-abstract class AbstractAppController extends AbstractPagesController
-{
-}
