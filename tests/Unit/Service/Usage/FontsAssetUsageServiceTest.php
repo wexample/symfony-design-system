b4d278183dbae02fe24019607cc4cece21a6ce34
weeger <romain.weeger@gmail.com>
2025-12-17 17:57:23 +0100

    Coverage

--- tests/Unit/Service/Usage/FontsAssetUsageServiceTest.php
+++ tests/Unit/Service/Usage/FontsAssetUsageServiceTest.php
@@ -7,6 +7,7 @@ use Wexample\SymfonyDesignSystem\Rendering\Asset;
 use Wexample\SymfonyDesignSystem\Rendering\AssetsRegistry;
 use Wexample\SymfonyDesignSystem\Rendering\RenderPass;
 use Wexample\SymfonyDesignSystem\Service\Usage\FontsAssetUsageService;
+use Wexample\WebRenderNode\Rendering\RenderNode\AbstractRenderNode;
 
 class FontsAssetUsageServiceTest extends TestCase
 {
@@ -53,5 +54,46 @@ class FontsAssetUsageServiceTest extends TestCase
         // Add a switchable usage to force hasExtraSwitchableUsage = true => cannot aggregate.
         $renderPass->usagesConfig[FontsAssetUsageService::getName()]['list']['alt'] = ['allow_switch' => true];
         $this->assertFalse($service->canAggregateAsset($renderPass, $asset));
+    }
+
+    public function testCreateAssetIfExistsThrowsWhenRealPathMissing(): void
+    {
+        $service = new FontsAssetUsageService();
+
+        $registry = $this->createStub(AssetsRegistry::class);
+        $registry->method('assetExists')->willReturn(true);
+        $registry->method('getRealPath')->willReturn(null);
+        $registry->method('getBuiltPath')->willReturn('path.css');
+
+        $renderPass = $this->createStub(RenderPass::class);
+        $renderPass->method('getAssetsRegistry')->willReturn($registry);
+
+        $renderNode = $this->createStub(AbstractRenderNode::class);
+        $renderNode->method('getContextType')->willReturn(Asset::CONTEXT_LAYOUT);
+        $renderNode->assets = [];
+
+        $this->expectException(\Exception::class);
+        $this->invokeCreateAssetIfExists($service, $renderPass, 'path.css', 'view', $renderNode);
+    }
+
+    /**
+     * @param FontsAssetUsageService $service
+     * @param RenderPass $renderPass
+     * @param string $path
+     * @param string $view
+     * @param AbstractRenderNode $renderNode
+     * @return mixed
+     */
+    private function invokeCreateAssetIfExists(
+        FontsAssetUsageService $service,
+        RenderPass $renderPass,
+        string $path,
+        string $view,
+        AbstractRenderNode $renderNode
+    ) {
+        $ref = new \ReflectionMethod($service, 'createAssetIfExists');
+        $ref->setAccessible(true);
+
+        return $ref->invoke($service, $renderPass, $path, $view, $renderNode);
     }
 }
