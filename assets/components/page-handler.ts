25ab0bf806e311ac85705234f8ab6296efc1f6c7
weeger <romain.weeger@gmail.com>
2025-12-28 16:57:14 +0100

    Empty

--- assets/components/page-handler.ts
+++
@@ -1,4 +0,0 @@
-import PageManagerComponent from "../js/class/PageManagerComponent";
-
-export default class PageHandlerComponent extends PageManagerComponent {
-}
--- assets/js/class/AssetUsage/Fonts.ts
+++
@@ -1,5 +0,0 @@
-import AssetUsage from '../AssetUsage';
-
-export default class extends AssetUsage {
-  public usageName: string = AssetUsage.USAGE_FONTS;
-}
