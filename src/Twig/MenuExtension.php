e572b1c388fc04078faaeace7c1e2e3375016313
weeger <romain.weeger@gmail.com>
2026-01-16 09:47:35 +0100

    Menu item

---
+++ src/Twig/MenuExtension.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wexample\SymfonyDesignSystem\Twig;
+
+use Twig\Environment;
+use Twig\TwigFunction;
+
+class MenuExtension extends AbstractTemplateExtension
+{
+    protected function getTemplateFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'menu_item',
+                function (Environment $twig, string $icon, string $label, string $href, array $options = []) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/menu-item.html.twig',
+                        [
+                            'icon' => $icon,
+                            'label' => $label,
+                            'href' => $href,
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+        ];
+    }
+}
