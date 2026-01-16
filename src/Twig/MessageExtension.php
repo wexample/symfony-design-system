010ca6a25f22cd897c16fdec0c260555b20bfc77
weeger <romain.weeger@gmail.com>
2026-01-16 20:17:38 +0100

    Message

---
+++ src/Twig/MessageExtension.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Wexample\SymfonyDesignSystem\Twig;
+
+use Twig\Environment;
+use Twig\TwigFunction;
+
+class MessageExtension extends AbstractTemplateExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction(
+                'message_info',
+                function (Environment $twig, string $title, ?string $body = null, array $options = []) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/message.html.twig',
+                        [
+                            'type' => 'info',
+                            'title' => $title,
+                            'body' => $body,
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+            new TwigFunction(
+                'message_success',
+                function (Environment $twig, string $title, ?string $body = null, array $options = []) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/message.html.twig',
+                        [
+                            'type' => 'success',
+                            'title' => $title,
+                            'body' => $body,
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+            new TwigFunction(
+                'message_warning',
+                function (Environment $twig, string $title, ?string $body = null, array $options = []) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/message.html.twig',
+                        [
+                            'type' => 'warning',
+                            'title' => $title,
+                            'body' => $body,
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+            new TwigFunction(
+                'message_error',
+                function (Environment $twig, string $title, ?string $body = null, array $options = []) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/message.html.twig',
+                        [
+                            'type' => 'error',
+                            'title' => $title,
+                            'body' => $body,
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+        ];
+    }
+}
