a4c124014d3df42ac3d1dffaf81c632a09a08cb5
weeger <romain.weeger@gmail.com>
2026-01-19 18:44:38 +0100

    Tab

--- src/Twig/TabExtension.php
+++ src/Twig/TabExtension.php
@@ -12,14 +12,36 @@ class TabExtension extends AbstractTemplateExtension
         return [
             new TwigFunction(
                 'tab_item',
-                function (Environment $twig, string $label, string $href, bool $active = false, array $options = []) {
+                function (
+                    Environment $twig,
+                    string $label,
+                    string $route,
+                    array $routeParams = [],
+                    array $options = []
+                ) {
+                    return $this->renderTemplate(
+                        $twig,
+                        '@WexampleSymfonyDesignSystemBundle/components/tab-item.html.twig',
+                        [
+                            'label' => $label,
+                            'route' => $route,
+                            'route_params' => $routeParams,
+                            'href' => $twig->getFunction('path')->getCallable()($route, $routeParams),
+                            'options' => $options,
+                        ]
+                    );
+                },
+                self::TEMPLATE_FUNCTION_OPTIONS
+            ),
+            new TwigFunction(
+                'tab_item_link',
+                function (Environment $twig, string $label, string $href, array $options = []) {
                     return $this->renderTemplate(
                         $twig,
                         '@WexampleSymfonyDesignSystemBundle/components/tab-item.html.twig',
                         [
                             'label' => $label,
                             'href' => $href,
-                            'active' => $active,
                             'options' => $options,
                         ]
                     );
