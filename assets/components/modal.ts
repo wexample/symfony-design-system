5638bcd76e67931d6c2947b2c630d440a1e416f7
weeger <romain.weeger@gmail.com>
2026-02-05 16:00:24 +0100

    Modal to design system

---
+++ assets/components/modal.ts
@@ -0,0 +1,69 @@
+import Page from '@wexample/symfony-loader/js/Class/Page';
+import PageManagerComponent from '@wexample/symfony-loader/js/Class/PageManagerComponent';
+import RenderNode from '@wexample/symfony-loader/js/Class/RenderNode';
+
+export default class extends PageManagerComponent {
+  private overlayEl?: HTMLElement;
+  private contentEl?: HTMLElement;
+
+  attachHtmlElements() {
+    super.attachHtmlElements();
+
+    this.overlayEl = this.el.querySelector('[data-modal-overlay]') as HTMLElement;
+    this.contentEl = this.el.querySelector('.modal--content') as HTMLElement;
+
+    if (this.contentEl && this.layoutBody) {
+      this.contentEl.innerHTML = this.layoutBody;
+    }
+  }
+
+  appendChildRenderNode(renderNode: RenderNode) {
+    super.appendChildRenderNode(renderNode);
+
+    if (renderNode instanceof Page) {
+      renderNode.ready(() => {
+        this.open();
+      });
+    }
+  }
+
+  public getPageEl(): HTMLElement {
+    return this.contentEl || this.el;
+  }
+
+  public setLayoutBody(body: string) {
+    super.setLayoutBody(body);
+
+    if (this.contentEl) {
+      this.contentEl.innerHTML = body || '';
+    }
+  }
+
+  protected async activateListeners(): Promise<void> {
+    await super.activateListeners();
+
+    this.overlayEl?.addEventListener('click', this.onClickOverlay);
+  }
+
+  protected async deactivateListeners(): Promise<void> {
+    this.overlayEl?.removeEventListener('click', this.onClickOverlay);
+
+    await super.deactivateListeners();
+  }
+
+  private open() {
+    this.el.classList.add('is-open');
+    this.page?.focus();
+  }
+
+  private async close() {
+    this.el.classList.remove('is-open');
+    this.page?.blur();
+    await this.exit();
+    this.callerPage?.focus();
+  }
+
+  private onClickOverlay = async () => {
+    await this.close();
+  };
+}
--- src/DependencyInjection/WexampleSymfonyDesignSystemExtension.php
+++ src/DependencyInjection/WexampleSymfonyDesignSystemExtension.php
@@ -3,10 +3,27 @@
 namespace Wexample\SymfonyDesignSystem\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Wexample\SymfonyHelpers\DependencyInjection\AbstractWexampleSymfonyExtension;
+use Wexample\SymfonyLoader\DependencyInjection\Traits\WithLoaderConfigurationExtensionTrait;
 
-class WexampleSymfonyDesignSystemExtension extends AbstractWexampleSymfonyExtension
+class WexampleSymfonyDesignSystemExtension extends AbstractWexampleSymfonyExtension implements PrependExtensionInterface
 {
+    use WithLoaderConfigurationExtensionTrait;
+
+    public function prepend(
+        ContainerBuilder $container
+    ): void {
+        $layoutBases = (array) ($container->hasParameter('wexample_symfony_design_system.loader.layout_bases')
+            ? $container->getParameter('wexample_symfony_design_system.loader.layout_bases')
+            : []);
+
+        $this->prependLoaderLayoutBases(
+            $container,
+            $this->normalizeLoaderLayoutBases($layoutBases)
+        );
+    }
+
     public function load(
         array $configs,
         ContainerBuilder $container
