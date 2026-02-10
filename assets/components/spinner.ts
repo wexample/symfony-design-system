6b61a14b0358d41cedb2d4ba43346922662f3394
weeger <romain.weeger@gmail.com>
2026-02-10 16:51:40 +0100

    Spinner

---
+++ assets/components/spinner.ts
@@ -0,0 +1,4 @@
+import Component from '@wexample/symfony-loader/js/Class/Component';
+
+export default class extends Component {
+}
---
+++ assets/vue/partials/spinner.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  template: '#vue-template-wexample-symfony-design-system-bundle-vue-partials-spinner',
+
+  props: {
+    size: {
+      type: String,
+      default: 'md'
+    },
+    variant: {
+      type: String,
+      default: 'default'
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    extraClass: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    spinnerClass() {
+      const classes = ['spinner', `spinner--${this.size}`];
+      if (this.variant && this.variant !== 'default') {
+        classes.push(`spinner--${this.variant}`);
+      }
+      if (this.extraClass) {
+        classes.push(this.extraClass);
+      }
+      return classes.join(' ');
+    },
+
+    hasLabel() {
+      return Boolean(this.label);
+    }
+  }
+};
+</script>
