9eed947aa160020c79d87f06c45d3d09749c2f37
weeger <romain.weeger@gmail.com>
2026-02-18 15:49:17 +0100

    Entity

--- src/Twig/EntityExtension.php
+++ src/Twig/EntityExtension.php
@@ -5,13 +5,15 @@ namespace Wexample\SymfonyDesignSystem\Twig;
 use Twig\Environment;
 use Twig\TwigFunction;
 use Wexample\Helpers\Class\Traits\HasSnakeShortClassNameClassTrait;
+use Wexample\SymfonyLoader\Rendering\RenderPass;
 use Wexample\SymfonyLoader\Twig\ComponentsExtension;
 
 class EntityExtension extends AbstractTemplateExtension
 {
     public function __construct(
         private readonly ComponentsExtension $componentsExtension
-    ) {
+    )
+    {
     }
 
     public function getFunctions(): array
@@ -19,7 +21,6 @@ class EntityExtension extends AbstractTemplateExtension
         $options = [
             self::FUNCTION_OPTION_IS_SAFE => self::FUNCTION_OPTION_IS_SAFE_VALUE_HTML,
             self::FUNCTION_OPTION_NEEDS_ENVIRONMENT => true,
-            self::FUNCTION_OPTION_NEEDS_CONTEXT => true,
         ];
 
         return [
@@ -42,20 +43,15 @@ class EntityExtension extends AbstractTemplateExtension
      */
     public function entity(
         Environment $twig,
-        array $context,
+        RenderPass $renderPass,
         mixed $entity,
         string $format = 'bar',
         array $options = []
-    ): string {
-        $renderPass = $context['render_pass'] ?? null;
-        if (! $renderPass) {
-            throw new \RuntimeException('The "entity" Twig function requires "render_pass" in context.');
-        }
-
-        $entityName = $options['entity_name'] ?? $this->resolveEntityName($entity);
+    ): string
+    {
         $componentPath = $options['component'] ?? sprintf(
             '@front/components/entity/%s/%s',
-            $entityName,
+            $entity::getSnakeShortClassName(),
             strtolower(trim($format))
         );
 
@@ -68,34 +64,5 @@ class EntityExtension extends AbstractTemplateExtension
                 'options' => $options['component_options'] ?? [],
             ]
         );
-    }
-
-    private function resolveEntityName(mixed $entity): string
-    {
-        if (is_array($entity) && isset($entity['_entity']) && is_string($entity['_entity'])) {
-            return $this->normalizeName($entity['_entity']);
-        }
-
-        if (is_object($entity)) {
-            $shortName = (new \ReflectionClass($entity))->getShortName();
-            return $this->normalizeName($shortName);
-        }
-
-        throw new \InvalidArgumentException(
-            'Unable to resolve entity name. Provide an object entity or options["entity_name"].'
-        );
-    }
-
-    private function normalizeName(string $name): string
-    {
-        $normalized = preg_replace('/Entity$/', '', trim($name)) ?? '';
-        $normalized = preg_replace('/([a-z])([A-Z])/', '$1-$2', $normalized) ?? '';
-        $normalized = strtolower($normalized);
-
-        if ($normalized === '') {
-            throw new \InvalidArgumentException('Entity name cannot be empty.');
-        }
-
-        return $normalized;
     }
 }
