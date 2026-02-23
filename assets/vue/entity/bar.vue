b09678c0b391981e91fc0fd4da11cee860fd897e
weeger <romain.weeger@gmail.com>
2026-02-23 16:33:05 +0100

    Bar entity

--- assets/vue/entity/bar.vue
+++ assets/vue/entity/bar.vue
@@ -4,6 +4,14 @@ import Entity from "./entity.vue";
 
 export default {
   extends: Entity,
+  methods: {
+    getWrapperCssClassDeclarations() {
+      return [
+        'entity',
+        'entity--display--bar'
+      ];
+    }
+  }
 }
 
 </script>
