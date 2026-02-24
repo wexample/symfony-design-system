21edf85c20c967cb1f42a72b2ce3fefa9578c42a
weeger <romain.weeger@gmail.com>
2026-02-24 10:29:16 +0100

    List expanded

---
+++ assets/vue/partials/list-expanded.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  template: '#vue-template-wexample-symfony-design-system-bundle-vue-partials-list-expanded',
+
+  props: {
+    showHeader: {
+      type: Boolean,
+      default: false
+    },
+    showFilterButton: {
+      type: Boolean,
+      default: false
+    },
+    searchPlaceholder: {
+      type: String,
+      required: true
+    },
+    filterLabel: {
+      type: String,
+      required: true
+    },
+    emptyLabel: {
+      type: String,
+      required: true
+    },
+    hasItems: {
+      type: Boolean,
+      required: true
+    },
+    searchValue: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    onSearchInput(event) {
+      this.$emit('search', event.target.value);
+    },
+
+    onFilterClick() {
+      this.$emit('filter-click');
+    }
+  }
+};
+</script>
