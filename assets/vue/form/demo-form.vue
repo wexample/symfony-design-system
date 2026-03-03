de84ac4f8e1c5a39040a3e684c563c13a538e052
weeger <romain.weeger@gmail.com>
2026-03-03 09:50:40 +0100

    Form

--- assets/vue/form/demo-form.vue
+++ assets/vue/form/demo-form.vue
@@ -4,6 +4,18 @@ import FormVue from "../bases/form.vue";
 
 export default {
   extends: FormVue,
+
+  data() {
+    return {
+      name: ''
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      console.log('SUBMIT!');
+    }
+  }
 }
 
 </script>
