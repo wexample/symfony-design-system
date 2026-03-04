7e22ccd79005ee6a58df901014b0f2b32ac55284
weeger <romain.weeger@gmail.com>
2026-03-04 11:35:21 +0100

    Fields

--- assets/vue/form/fields/text-input.vue
+++ assets/vue/form/fields/text-input.vue
@@ -1,17 +1,12 @@
 <script>
+import BaseField from './base-field.vue';
+
 export default {
-  template: '#vue-template-wexample-symfony-design-system-bundle-vue-form-text-input',
+  extends: BaseField,
+  template: '#vue-template-wexample-symfony-design-system-bundle-vue-form-fields-text-input',
 
   props: {
     value: {
-      type: String,
-      default: ''
-    },
-    name: {
-      type: String,
-      default: ''
-    },
-    id: {
       type: String,
       default: ''
     },
@@ -19,51 +14,9 @@ export default {
       type: String,
       default: 'text'
     },
-    label: {
-      type: String,
-      default: ''
-    },
     placeholder: {
       type: String,
       default: ''
-    },
-    errors: {
-      type: Array,
-      default: () => []
-    },
-    errorTranslationPrefix: {
-      type: String,
-      default: ''
-    },
-    required: {
-      type: Boolean,
-      default: false
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    autocomplete: {
-      type: String,
-      default: ''
-    }
-  },
-
-  computed: {
-    hasErrors() {
-      return Array.isArray(this.errors) && this.errors.length > 0;
-    },
-
-    hasLabel() {
-      return Boolean(this.label);
-    },
-
-    hasErrorTranslationPrefix() {
-      return Boolean(this.errorTranslationPrefix);
-    },
-
-    resolvedId() {
-      return this.id || this.name || null;
     }
   },
 
