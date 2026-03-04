30ad718ae9dcd2f69b39560ec8c02aac737cb585
weeger <romain.weeger@gmail.com>
2026-03-04 15:18:17 +0100

    Fix

--- assets/vue/bases/vue-wrapper.vue
+++ assets/vue/bases/vue-wrapper.vue
@@ -1,10 +1,12 @@
 <script>
 
 import WithCssClassesVueMixin from "../../js/Vue/WithCssClassesVueMixin";
+import AbstractDesignSystemVueMixin from "../../js/Vue/AbstractDesignSystemVueMixin";
 
 export default {
   mixins: [
-    WithCssClassesVueMixin
+    WithCssClassesVueMixin,
+    AbstractDesignSystemVueMixin
   ],
   template: '#vue-template-wexample-symfony-design-system-bundle-vue-partials-vue-wrapper',
 };
