986a4e1cd9f3aa1d59d1324e734a4057271e1628
weeger <romain.weeger@gmail.com>
2026-07-24 08:24:59 +0200

    Various

--- assets/components/button-modal.ts
+++ assets/components/button-modal.ts
@@ -1,52 +1,14 @@
-import Component from '@wexample/symfony-loader/js/Class/Component';
 import ModalService from '@wexample/symfony-loader/js/Services/ModalService';
-import { locationHashParamGet, locationHashParamSet } from '@wexample/js-helpers/Helper/Location';
+import AbstractOverlayButton from '../js/Class/AbstractOverlayButton';
 
 export const HASH_KEY_MODAL = 'modal';
 export const HASH_KEY_MODAL_OPTS = 'modal.opts';
 export const HASH_KEY_MODAL_PAGE = 'modal.page';
 
-export default class extends Component {
-  private linkEl?: HTMLAnchorElement;
-
-  protected async mounted(): Promise<void> {
-    this.linkEl = this.el as HTMLAnchorElement;
-    this.linkEl.addEventListener('click', this.onClick);
-    await super.mounted();
-
-    const savedUrl = locationHashParamGet(HASH_KEY_MODAL);
-    const savedPage = locationHashParamGet(HASH_KEY_MODAL_PAGE);
-    const href = this.linkEl?.getAttribute('href');
-
-    if (savedUrl && href && savedUrl === href && savedPage === window.location.pathname) {
-      const savedOptsRaw = locationHashParamGet(HASH_KEY_MODAL_OPTS);
-      const opts = savedOptsRaw ? JSON.parse(savedOptsRaw) : {};
-      this.openModal(href, opts);
-    }
-  }
-
-  protected async unmounted(): Promise<void> {
-    this.linkEl?.removeEventListener('click', this.onClick);
-    await super.unmounted();
-  }
-
-  private openModal(href: string, options: Record<string, any>): void {
-    locationHashParamSet(HASH_KEY_MODAL, href, true);
-    locationHashParamSet(HASH_KEY_MODAL_PAGE, window.location.pathname, true);
-    if (Object.keys(options).length) {
-      locationHashParamSet(HASH_KEY_MODAL_OPTS, JSON.stringify(options), true);
-    }
+export default class extends AbstractOverlayButton {
+  protected getHashKeys(): [string, string, string] { return [HASH_KEY_MODAL, HASH_KEY_MODAL_OPTS, HASH_KEY_MODAL_PAGE]; }
+  protected getOptionsAttribute(): string { return 'data-modal-options'; }
+  protected openOverlay(href: string, options: Record<string, any>): void {
     (this.app.getService(ModalService) as ModalService).get(href, options);
   }
-
-  private onClick = (event: Event) => {
-    const href = this.linkEl?.getAttribute('href');
-    if (!href || href === '#') {
-      return;
-    }
-    event.preventDefault();
-    const optionsRaw = this.linkEl?.getAttribute('data-modal-options');
-    const options = optionsRaw ? JSON.parse(optionsRaw) : {};
-    this.openModal(href, options);
-  };
 }
--- assets/components/button-panel.ts
+++ assets/components/button-panel.ts
@@ -1,52 +1,14 @@
-import Component from '@wexample/symfony-loader/js/Class/Component';
 import PanelService from '@wexample/symfony-loader/js/Services/PanelService';
-import { locationHashParamGet, locationHashParamSet } from '@wexample/js-helpers/Helper/Location';
+import AbstractOverlayButton from '../js/Class/AbstractOverlayButton';
 
 export const HASH_KEY_PANEL = 'panel';
 export const HASH_KEY_PANEL_OPTS = 'panel.opts';
 export const HASH_KEY_PANEL_PAGE = 'panel.page';
 
-export default class extends Component {
-  private linkEl?: HTMLAnchorElement;
-
-  protected async mounted(): Promise<void> {
-    this.linkEl = this.el as HTMLAnchorElement;
-    this.linkEl.addEventListener('click', this.onClick);
-    await super.mounted();
-
-    const savedUrl = locationHashParamGet(HASH_KEY_PANEL);
-    const savedPage = locationHashParamGet(HASH_KEY_PANEL_PAGE);
-    const href = this.linkEl?.getAttribute('href');
-
-    if (savedUrl && href && savedUrl === href && savedPage === window.location.pathname) {
-      const savedOptsRaw = locationHashParamGet(HASH_KEY_PANEL_OPTS);
-      const opts = savedOptsRaw ? JSON.parse(savedOptsRaw) : {};
-      this.openPanel(href, opts);
-    }
-  }
-
-  protected async unmounted(): Promise<void> {
-    this.linkEl?.removeEventListener('click', this.onClick);
-    await super.unmounted();
-  }
-
-  private openPanel(href: string, options: Record<string, any>): void {
-    locationHashParamSet(HASH_KEY_PANEL, href, true);
-    locationHashParamSet(HASH_KEY_PANEL_PAGE, window.location.pathname, true);
-    if (Object.keys(options).length) {
-      locationHashParamSet(HASH_KEY_PANEL_OPTS, JSON.stringify(options), true);
-    }
+export default class extends AbstractOverlayButton {
+  protected getHashKeys(): [string, string, string] { return [HASH_KEY_PANEL, HASH_KEY_PANEL_OPTS, HASH_KEY_PANEL_PAGE]; }
+  protected getOptionsAttribute(): string { return 'data-panel-options'; }
+  protected openOverlay(href: string, options: Record<string, any>): void {
     (this.app.getService(PanelService) as PanelService).get(href, options);
   }
-
-  private onClick = (event: Event) => {
-    const href = this.linkEl?.getAttribute('href');
-    if (!href || href === '#') {
-      return;
-    }
-    event.preventDefault();
-    const optionsRaw = this.linkEl?.getAttribute('data-panel-options');
-    const options = optionsRaw ? JSON.parse(optionsRaw) : {};
-    this.openPanel(href, options);
-  };
 }
