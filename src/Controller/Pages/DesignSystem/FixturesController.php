bab15c5e4dba5367fb4214aafce5a5dc2c4b9b01
weeger <romain.weeger@gmail.com>
2026-07-24 15:45:12 +0200

    Cleanup

---
+++ src/Controller/Pages/DesignSystem/FixturesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wexample\SymfonyDesignSystem\Controller\Pages\DesignSystem;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Wexample\SymfonyLoader\Controller\Pages\AbstractDesignSystemController;
+
+#[Route(
+    name: 'wexample_design_system_fixtures_',
+    path: AbstractDesignSystemController::CONTROLLER_BASE_ROUTE . '/fixtures/',
+)]
+class FixturesController extends AbstractController
+{
+    private string $assetsDir = __DIR__ . '/../../../../assets/fixtures/';
+
+    #[Route('placeholder.svg', name: 'placeholder_svg')]
+    public function placeholderSvg(): Response
+    {
+        return new Response(
+            file_get_contents($this->assetsDir . 'placeholder.svg'),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'image/svg+xml',
+                'Cache-Control' => 'public, max-age=86400',
+            ]
+        );
+    }
+}
