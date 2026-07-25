d6b632c98f21414967297c885d62044e83de5454
weeger <romain.weeger@gmail.com>
2026-07-25 20:30:46 +0200

    Cleanup

---
+++ assets/components/form/switch_input.ts
@@ -0,0 +1,21 @@
+import Component from '@wexample/symfony-loader/js/Class/Component';
+
+export default class extends Component {
+  private checkboxEl?: HTMLInputElement;
+  private switchEl?: HTMLElement;
+
+  protected async activateListeners(): Promise<void> {
+    this.checkboxEl = this.el.querySelector('.switch--checkbox') as HTMLInputElement;
+    this.switchEl = this.el.querySelector('.switch') as HTMLElement;
+
+    this.checkboxEl?.addEventListener('change', this.onCheckboxChange);
+  }
+
+  protected async deactivateListeners(): Promise<void> {
+    this.checkboxEl?.removeEventListener('change', this.onCheckboxChange);
+  }
+
+  private onCheckboxChange = (): void => {
+    this.switchEl?.setAttribute('data-checked', String(this.checkboxEl?.checked ?? false));
+  };
+}
--- src/Form/Demo/FormSubmitBehaviorDemoForm.php
+++ src/Form/Demo/FormSubmitBehaviorDemoForm.php
@@ -15,6 +15,7 @@ use Wexample\SymfonyForms\Form\Type\TimeInputType;
 use Wexample\SymfonyForms\Form\Type\UrlInputType;
 use Wexample\SymfonyForms\Form\Type\RadioInputType;
 use Wexample\SymfonyForms\Form\Type\SelectInputType;
+use Wexample\SymfonyForms\Form\Type\SwitchInputType;
 use Wexample\SymfonyForms\Form\Type\TextareaInputType;
 use Wexample\SymfonyForms\Form\Type\TextInputType;
 use Wexample\SymfonyLoader\Helper\AdaptiveRequestHelper;
@@ -98,6 +99,15 @@ class FormSubmitBehaviorDemoForm extends AbstractForm
                     self::FIELD_OPTION_NAME_REQUIRED => false,
                     self::FIELD_OPTION_NAME_MAPPED => false,
                     'choices' => ['option_a', 'option_b', 'option_c'],
+                ]
+            )
+            ->add(
+                'switch',
+                SwitchInputType::class,
+                [
+                    self::FIELD_OPTION_NAME_LABEL => true,
+                    self::FIELD_OPTION_NAME_REQUIRED => false,
+                    self::FIELD_OPTION_NAME_MAPPED => false,
                 ]
             )
             ->add(
