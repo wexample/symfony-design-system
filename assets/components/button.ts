5a5415b56400d3d46ca61ea0cfb687952c19c425
weeger <romain.weeger@gmail.com>
2026-07-25 11:42:05 +0200

    Cleanup

--- assets/components/button.ts
+++ assets/components/button.ts
@@ -1,5 +1,19 @@
 import Component from '@wexample/symfony-loader/js/Class/Component';
 
 export default class extends Component {
+  activateListeners(): void {
+    super.activateListeners();
 
+    const button = this.el as HTMLButtonElement;
+    if (button.type === 'submit' && button.form) {
+      button.form.addEventListener('submit', () => this.setLoading(true));
+    }
+  }
+
+  setLoading(loading: boolean): void {
+    const button = this.el as HTMLButtonElement;
+    button.classList.toggle('is-loading', loading);
+    button.disabled = loading;
+    button.setAttribute('aria-busy', loading ? 'true' : 'false');
+  }
 }
