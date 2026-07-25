6ffb4c5369026a79c2ce4429cc251be35b34fbfa
weeger <romain.weeger@gmail.com>
2026-07-25 20:25:33 +0200

    Cleanup

--- src/Form/Demo/FormSubmitBehaviorDemoForm.php
+++ src/Form/Demo/FormSubmitBehaviorDemoForm.php
@@ -9,6 +9,7 @@ use Wexample\SymfonyForms\Form\AbstractForm;
 use Wexample\SymfonyForms\Form\Type\DateInputType;
 use Wexample\SymfonyForms\Form\Type\DatetimeInputType;
 use Wexample\SymfonyForms\Form\Type\EmailInputType;
+use Wexample\SymfonyForms\Form\Type\FileInputType;
 use Wexample\SymfonyForms\Form\Type\NumberInputType;
 use Wexample\SymfonyForms\Form\Type\TimeInputType;
 use Wexample\SymfonyForms\Form\Type\UrlInputType;
@@ -120,6 +121,15 @@ class FormSubmitBehaviorDemoForm extends AbstractForm
             ->add(
                 'time',
                 TimeInputType::class,
+                [
+                    self::FIELD_OPTION_NAME_LABEL => true,
+                    self::FIELD_OPTION_NAME_REQUIRED => false,
+                    self::FIELD_OPTION_NAME_MAPPED => false,
+                ]
+            )
+            ->add(
+                'file',
+                FileInputType::class,
                 [
                     self::FIELD_OPTION_NAME_LABEL => true,
                     self::FIELD_OPTION_NAME_REQUIRED => false,
