1a0c2b14800cceced3002240f08a81543bcb7cb4
weeger <romain.weeger@gmail.com>
2026-07-25 21:59:09 +0200

    Cleanup

--- assets/vue/bases/form-field.vue
+++ assets/vue/bases/form-field.vue
@@ -1,5 +1,9 @@
 <script>
 export default {
+  inject: {
+    formController: { default: null }
+  },
+
   props: {
     name: {
       type: String,
@@ -39,9 +43,26 @@ export default {
     }
   },
 
+  data() {
+    return {
+      controllerDisabled: false,
+      controllerErrors: [],
+    };
+  },
+
   computed: {
+    isDisabled() {
+      return this.disabled || this.controllerDisabled;
+    },
+
+    resolvedErrors() {
+      const propErrors = Array.isArray(this.errors) ? this.errors : [];
+      const ctrlErrors = Array.isArray(this.controllerErrors) ? this.controllerErrors : [];
+      return [...propErrors, ...ctrlErrors];
+    },
+
     hasErrors() {
-      return Array.isArray(this.errors) && this.errors.length > 0;
+      return this.resolvedErrors.length > 0;
     },
 
     hasLabel() {
@@ -54,6 +75,26 @@ export default {
 
     resolvedId() {
       return this.id || this.name || null;
+    }
+  },
+
+  mounted() {
+    if (this.formController && this.name) {
+      this._fieldProxy = {
+        fieldName: this.name,
+        disable: () => { this.controllerDisabled = true; },
+        enable: () => { this.controllerDisabled = false; },
+        setErrors: (errors) => { this.controllerErrors = [...errors]; },
+        clearErrors: () => { this.controllerErrors = []; },
+      };
+      this.formController.registerField(this._fieldProxy);
+    }
+  },
+
+  beforeUnmount() {
+    if (this._fieldProxy) {
+      this.formController?.unregisterField(this._fieldProxy);
+      this._fieldProxy = null;
     }
   },
 
