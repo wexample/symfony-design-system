79053ae3eed0d2a668b6ebb7937bcb3014091b2c
weeger <romain.weeger@gmail.com>
2026-07-25 20:46:35 +0200

    Cleanup

--- assets/components/form/submit_input.ts
+++ assets/components/form/submit_input.ts
@@ -1,3 +1,18 @@
 import Component from '@wexample/symfony-loader/js/Class/Component';
 
-export default class extends Component {}
+export default class extends Component {
+  private formEl: HTMLFormElement | null = null;
+
+  protected async activateListeners(): Promise<void> {
+    this.formEl = this.el.closest('form');
+    this.formEl?.addEventListener('submit', this.onFormSubmit);
+  }
+
+  protected async deactivateListeners(): Promise<void> {
+    this.formEl?.removeEventListener('submit', this.onFormSubmit);
+  }
+
+  private onFormSubmit = (): void => {
+    this.el.classList.add('is-loading');
+  };
+}
---
+++ assets/js/Class/Field.ts
@@ -0,0 +1,40 @@
+import Component from '@wexample/symfony-loader/js/Class/Component';
+
+export default abstract class Field extends Component {
+  private formEl: HTMLFormElement | null = null;
+
+  protected async activateListeners(): Promise<void> {
+    await super.activateListeners();
+
+    this.formEl = this.el.closest('form');
+    this.formEl?.addEventListener('submit', this.onFormSubmit);
+  }
+
+  protected async deactivateListeners(): Promise<void> {
+    await super.deactivateListeners();
+
+    this.formEl?.removeEventListener('submit', this.onFormSubmit);
+  }
+
+  private onFormSubmit = (): void => {
+    this.disable();
+  };
+
+  public disable(): void {
+    this.el
+      .querySelectorAll('input:not([type="submit"]), textarea, select, button:not([type="submit"])')
+      .forEach((el) => {
+        (el as HTMLInputElement).disabled = true;
+      });
+    this.el.classList.add('is-disabled');
+  }
+
+  public enable(): void {
+    this.el
+      .querySelectorAll('input:not([type="submit"]), textarea, select, button:not([type="submit"])')
+      .forEach((el) => {
+        (el as HTMLInputElement).disabled = false;
+      });
+    this.el.classList.remove('is-disabled');
+  }
+}
