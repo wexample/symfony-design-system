e0a009f196e6cf74a37b070bec60eb54095f94b3
weeger <romain.weeger@gmail.com>
2026-07-26 10:39:16 +0200

    Cleanup

---
+++ assets/vue/form/fields/number-input.vue
@@ -0,0 +1,22 @@
+<script>
+import BaseField from '../../bases/form-field.vue';
+
+export default {
+  extends: BaseField,
+  template: '#vue-template-wexample-symfony-design-system-bundle-vue-form-fields-number-input',
+  emits: ['update:modelValue'],
+
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    onInput(event) {
+      this.$emit('update:modelValue', event?.target?.value ?? '');
+    }
+  }
+};
+</script>
---
+++ assets/vue/form/fields/password-input.vue
@@ -0,0 +1,26 @@
+<script>
+import BaseField from '../../bases/form-field.vue';
+
+export default {
+  extends: BaseField,
+  template: '#vue-template-wexample-symfony-design-system-bundle-vue-form-fields-password-input',
+  emits: ['update:modelValue'],
+
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    onInput(event) {
+      this.$emit('update:modelValue', event?.target?.value ?? '');
+    }
+  }
+};
+</script>
