4a85140a2de69f528c381cad67e63437a37dc4d4
ai-editor agent <agent.ai-editor@wex.local>
2026-08-28 10:59:32 +0200

    refactor: move design system showcase to symfony-design-system-demo

--- assets/forms/demo/form_submit_behavior_ajax_demo_form.ts
+++
@@ -1,3 +0,0 @@
-import FormSubmitBehavior from './form_submit_behavior_demo_form';
-
-export default class extends FormSubmitBehavior {}
--- assets/pages/design_system/generic/dialog/index.ts
+++
@@ -1,5 +0,0 @@
-import Page from '@wexample/symfony-loader/js/Class/Page';
-
-export default class extends Page {
-
-}
